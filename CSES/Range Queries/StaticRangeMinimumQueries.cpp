0fc2a2bf35f9bb4af07566f204e34cc96e5a357a
codersanjeev <thedevelopersanjeev@gmail.com>
2021-08-04 00:04:07 +0530

    added new questions

--- CSES/Range Queries/StaticRangeMinimumQueries.cpp
+++ CSES/Range Queries/StaticRangeMinimumQueries.cpp
@@ -1,5 +1,55 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+
 #include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define MOD 1000000007LL
+#define PI acos(-1)
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename... T>
+void read(T &...args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&...args) {
+    ((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+    for (auto &e : t) {
+        read(e);
+    }
+}
+
+template <typename T>
+void writeContainer(T &t) {
+    for (const auto &e : t) {
+        write(e, " ");
+    }
+    write("\n");
+}
 
 template <typename T>
 class SegmentTree {
@@ -32,7 +82,7 @@ class SegmentTree {
     }
 
     T query(int l, int r, int x, int lx, int rx) {
-        if (lx >= r || l >= rx) return 0;
+        if (lx >= r || l >= rx) return 1e18;
         if (lx >= l && rx <= r) return _tree[x];
         int m = lx + (rx - lx) / 2;
         int left = query(l, r, 2 * x + 1, lx, m);
@@ -41,7 +91,7 @@ class SegmentTree {
     }
 
     T merge(T left, T right) {
-        return __gcd(left, right);
+        return min(left, right);
     }
 
    public:
@@ -61,3 +111,25 @@ class SegmentTree {
         return query(l, r, 0, 0, _size);
     }
 };
+
+void solve() {
+    int n, q, a, b;
+    read(n, q);
+    vector<int> arr(n);
+    readContainer(arr);
+    SegmentTree<int> tree(arr);
+    while (q--) {
+        read(a, b);
+        write(tree.query(a - 1, b), "\n");
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    // read(T);
+    for (int t = 1; t <= T; ++t) {
+        solve();
+    }
+}
--- CSES/Range Queries/StaticRangeSumQueries.cpp
+++ CSES/Range Queries/StaticRangeSumQueries.cpp
@@ -1,5 +1,55 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("fast-math")
+
 #include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+#define MOD 1000000007LL
+#define PI acos(-1)
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename... T>
+void read(T &...args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&...args) {
+    ((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+    for (auto &e : t) {
+        read(e);
+    }
+}
+
+template <typename T>
+void writeContainer(T &t) {
+    for (const auto &e : t) {
+        write(e, " ");
+    }
+    write("\n");
+}
 
 template <typename T>
 class SegmentTree {
@@ -41,7 +91,7 @@ class SegmentTree {
     }
 
     T merge(T left, T right) {
-        return __gcd(left, right);
+        return left + right;
     }
 
    public:
@@ -61,3 +111,25 @@ class SegmentTree {
         return query(l, r, 0, 0, _size);
     }
 };
+
+void solve() {
+    int n, q, a, b;
+    read(n, q);
+    vector<int> arr(n);
+    readContainer(arr);
+    SegmentTree<int> tree(arr);
+    while (q--) {
+        read(a, b);
+        write(tree.query(a - 1, b), "\n");
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    // read(T);
+    for (int t = 1; t <= T; ++t) {
+        solve();
+    }
+}
