33bd1e7b8b8d51d53c14a6e5bbc30c2be9e1b755
codersanjeev <thedevelopersanjeev@gmail.com>
2021-12-26 01:21:55 +0530

    added abc233

---
+++ AtCoder/abc233/C.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+
+const int mod = 1000000007LL;
+const long double pi = acos(-1);
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename... T>
+void read(T &...args) {
+	((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&...args) {
+	((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+	for (auto &e : t) {
+		read(e);
+	}
+}
+
+template <typename T>
+void writeContainer(T &t) {
+	for (const auto &e : t) {
+		write(e, " ");
+	}
+	write("\n");
+}
+
+int ans = 0;
+
+void findCount(const vector<vector<int>> &balls, int x, int i, int curr) {
+	if (i == balls.size()) {
+		ans += (curr == x);
+		return;
+	}
+	for (const auto &ball : balls[i]) {
+		if (curr * ball > x) continue;
+		findCount(balls, x, i + 1, curr * ball);
+	}
+}
+
+void solve(int tc) {
+	int n, x;
+	read(n, x);
+	vector<vector<int>> balls(n);
+	for (int i = 0; i < n; ++i) {
+		int li;
+		read(li);
+		vector<int> curr(li);
+		readContainer(curr);
+		balls[i] = curr;
+	}
+	findCount(balls, x, 0, 1);
+	write(ans);
+}
+
+signed main() {
+	auto start = chrono::high_resolution_clock::now();
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T = 1;
+	// read(T);
+	for (int t = 1; t <= T; ++t) {
+		solve(t);
+	}
+	auto end = chrono::high_resolution_clock::now();
+	chrono::duration<double, milli> duration = end - start;
+	// write("Time Taken = ", duration.count(), " ms\n");
+}
