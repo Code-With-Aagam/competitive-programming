adbc2676747c8aec760daa748a139540bb2e4761
codersanjeev <thedevelopersanjeev@gmail.com>
2021-12-26 18:16:53 +0530

    updated template

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 using namespace __gnu_pbds;
 
 #define deb(x) cout << #x << " is " << x << "\n"
-// #define int long long
+#define int long long
 
 const int mod = 1000000007;
 const int inf = 1e18;
@@ -46,45 +46,8 @@ void writeContainer(T &t) {
 	write("\n");
 }
 
-/**
- * Recursive -> TLE
- */
-int countNumberOfWays(const vector<int> &coins, int index, int X) {
-	if (index >= coins.size()) {
-		return (X == 0 ? 1 : 0);
-	} else if (X < 0) {
-		return 0;
-	} else if (X == 0) {
-		return 1;
-	} else {
-		int includingCurrentCoin = countNumberOfWays(coins, index, X - coins[index]);
-		int excludingCurrentCoin = countNumberOfWays(coins, index + 1, X);
-		return includingCurrentCoin + excludingCurrentCoin;
-	}
-}
-
 void solve() {
-	int n, X;
-	read(n, X);
-	vector<int> coins(n);
-	readContainer(coins);
-	vector<vector<int>> ways(n, vector<int>(X + 1, 0));
-	for (int i = 0; i < n; ++i) {
-		ways[i][0] = 1;
-	}
-	for (int i = 0; i < n; ++i) {
-		for (int x = 1; x <= X; ++x) {
-			if (x - coins[i] >= 0) {
-				ways[i][x] += ways[i][x - coins[i]];
-				ways[i][x] %= mod;
-			}
-			if (i - 1 >= 0) {
-				ways[i][x] += ways[i - 1][x];
-				ways[i][x] %= mod;
-			}
-		}
-	}
-	write(ways.back().back(), "\n");
+
 }
 
 signed main() {
@@ -92,11 +55,11 @@ signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	int T = 1;
-	// read(T);
+	read(T);
 	for (int t = 1; t <= T; ++t) {
 		solve();
 	}
 	auto end = chrono::high_resolution_clock::now();
 	chrono::duration<double, milli> duration = end - start;
-	// write("Time Taken = ", duration.count(), " ms\n");
+	write("Time Taken = ", duration.count(), " ms\n");
 }
