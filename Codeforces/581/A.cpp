51016193b1d2066c226bdc1f0d8f758d39099e78
codersanjeev <thedevelopersanjeev@gmail.com>
2022-01-04 14:51:46 +0530

    added new questions

---
+++ Codeforces/581/A.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(x) begin(x), end(x)
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+
+constexpr int mod = 1e9 + 7;
+constexpr int inf = 1e18;
+constexpr long double eps = 1e-9;
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename... T>
+void read(T &...args) {
+	((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&...args) {
+	((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+	for (auto &e : t) {
+		read(e);
+	}
+}
+
+template <typename T>
+void writeContainer(T &t) {
+	for (const auto &e : t) {
+		write(e, " ");
+	}
+	write("\n");
+}
+
+// (1 / x) % mod = binpow(x, mod - 2)
+int binpow(int x, int y) {
+	x %= mod;
+	int res = 1;
+	while (y > 0) {
+		if (y & 1) res = res * x;
+		res %= mod;
+		x *= x;
+		x %= mod;
+		y >>= 1;
+	}
+	return res;
+}
+
+void solve() {
+	int a, b;
+	read(a, b);
+	int ans = min(a, b);
+	a -= ans;
+	b -= ans;
+	write(ans, " ", (a / 2) + (b / 2));
+}
+
+signed main() {
+	auto start = chrono::high_resolution_clock::now();
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T = 1;
+	// read(T);
+	for (int t = 1; t <= T; ++t) {
+		solve();
+	}
+	auto end = chrono::high_resolution_clock::now();
+	chrono::duration<double, milli> duration = end - start;
+	// write("Time Taken = ", duration.count(), " ms\n");
+}
