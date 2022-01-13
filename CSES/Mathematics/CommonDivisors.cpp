5c31d0b7d0701365367c61f83ea86bac76d763be
codersanjeev <thedevelopersanjeev@gmail.com>
2022-01-13 21:01:03 +0530

    added common divisors

---
+++ CSES/Mathematics/CommonDivisors.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define all(x) begin(x), end(x)
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+ 
+constexpr int mod = 1e9 + 7;
+constexpr int infinity = 1e18;
+constexpr long double epsilon = 1e-9;
+ 
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+ 
+template <typename T>
+using max_heap = priority_queue<T>;
+ 
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+template <typename... T>
+void read(T &...args) {
+	((cin >> args), ...);
+}
+ 
+template <typename... T>
+void write(T &&...args) {
+	((cout << args), ...);
+}
+ 
+template <typename T>
+void readContainer(T &t) {
+	for (auto &e : t) {
+		read(e);
+	}
+}
+ 
+template <typename T>
+void writeContainer(T &t) {
+	for (const auto &e : t) {
+		write(e, " ");
+	}
+	write("\n");
+}
+ 
+// (1 / x) % mod = binpow(x, mod - 2)
+int binpow(int x, int y, int mod = mod) {
+	x %= mod;
+	int res = 1;
+	while (y > 0) {
+		if (y & 1) res = res * x;
+		res %= mod;
+		x *= x;
+		x %= mod;
+		y >>= 1;
+	}
+	return res;
+}
+ 
+void solve() {
+	int n;
+	read(n);
+	vector<int> x(n);
+	readContainer(x);
+	const int N = 1e6 + 1;
+	vector<int> cnt(N, 0);
+	for (const auto &ele : x) cnt[ele]++;
+	for (int ans = N - 1; ans >= 2; --ans) {
+		int nums = 0;
+		for (int y = ans; y < N; y += ans) {
+			nums += cnt[y];
+		}
+		if (nums >= 2) {
+			write(ans);
+			break;
+		}
+	}
+}
+ 
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T = 1;
+	// read(T);
+	for (int t = 1; t <= T; ++t) {
+		solve();
+	}
+}
