c09e92910a687f1409e6f32056bba9a652a02825
codersanjeev <thedevelopersanjeev@gmail.com>
2022-01-17 23:44:00 +0530

    added new questions

---
+++ Codeforces/255/A.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(x) begin(x), end(x)
+#define deb(x) cout << #x << " is " << x << "\n"
+#define int long long
+
+constexpr int mod = 1e9 + 7;
+constexpr int infinity = 1e18;
+constexpr long double epsilon = 1e-9;
+
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T>
+using max_heap = priority_queue<T>;
+
+template <class T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename... T>
+void read(T &...args) {
+	((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&...args) {
+	((cout << args), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+	for (auto &e : t) {
+		read(e);
+	}
+}
+
+template <typename T>
+void writeContainer(T &t) {
+	for (const auto &e : t) {
+		write(e, " ");
+	}
+	write("\n");
+}
+
+void solve() {
+	int n;
+	read(n);
+	vector<int> arr(n);
+	readContainer(arr);
+	vector<int> freq(3, 0);
+	for (int i = 0; i < n; ++i) {
+		freq[i % 3] += arr[i];
+	}
+	int mx = *max_element(all(freq));
+	if (mx == freq[0]) {
+		write("chest");
+	} else if (mx == freq[1]) {
+		write("biceps");
+	} else {
+		write("back");
+	}
+}
+
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int T = 1;
+	// read(T);
+	for (int t = 1; t <= T; ++t) {
+		solve();
+	}
+}
